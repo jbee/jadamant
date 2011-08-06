23a8f199df4fa86820ed464b766b1f4dd97730a8
Jan Bernitt <jan.bernitt@gmx.de>
2011-08-06 21:57:30 +0200

    NEW more Ord utilities in Order for Date, Calendar, HashCode and suborders (nested orders)

--- src/main/java/de/jbee/core/type/Order.java
+++ src/main/java/de/jbee/core/type/Order.java
@@ -1,7 +1,11 @@
 package de.jbee.core.type;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Comparator;
+import java.util.Date;
 
 import de.jbee.core.dev.Null;
 import de.jbee.core.dev.Nullproof;
@@ -15,10 +19,6 @@ import de.jbee.core.dev.Nullsave;
  * 
  */
 public final class Order {
-
-	private Order() {
-		throw new UnsupportedOperationException( "util" );
-	}
 
 	/**
 	 * <p>
@@ -40,13 +40,19 @@ public final class Order {
 	public static final Ord<CharSequence> alphabetical = new AlphabeticalOrder();
 	public static final Ord<Object> identity = new IdentityOrder();
 	public static final Ord<java.lang.Enum<?>> enumerative = new EnumerativeOrder();
+	public static final Ord<Date> chronological = new ChronologicalOrder();
+	public static final Ord<Calendar> calendrical = new CalendricalOrder();
+	public static final Ord<Object> byHashCode = new HashCodeOrder();
+
+	public static <T> Ord<T> asc( Ord<T> ord ) {
+		if ( ord instanceof InverseOrder<?> ) {
+			return asc( ( (InverseOrder<T>) ord ).ord );
+		}
+		return ord;
+	}
 
 	public static <T extends Comparable<T>> Ord<T> byCompare() {
 		return new CompareableOrder<T>();
-	}
-
-	public static <T> Ord<Object> typesave( Ord<? super T> ord, Class<T> type ) {
-		return new TypesaveOrder<T>( type, ord );
 	}
 
 	public static <T> Ord<T> desc( Ord<T> ord ) {
@@ -54,13 +60,6 @@ public final class Order {
 			return desc( ( (InverseOrder<T>) ord ).ord );
 		}
 		return new InverseOrder<T>( ord );
-	}
-
-	public static <T> Ord<T> asc( Ord<T> ord ) {
-		if ( ord instanceof InverseOrder<?> ) {
-			return asc( ( (InverseOrder<T>) ord ).ord );
-		}
-		return ord;
 	}
 
 	public static <T> Ord<T> inverse( Ord<T> ord ) {
@@ -88,19 +87,122 @@ public final class Order {
 		Arrays.sort( array, new OrderComparator<T>( ord ) );
 	}
 
-	static final class OrderComparator<T>
-			implements Comparator<T> {
+	public static <T> Ord<T> sub( Ord<? super T> primary, Ord<? super T> secondary ) {
+		return new SubOrder<T>( primary, secondary );
+	}
 
-		private final Ord<T> ord;
+	public static <T> Ord<Object> typesave( Ord<? super T> ord, Class<T> type ) {
+		return new TypesaveOrder<T>( type, ord );
+	}
 
-		OrderComparator( Ord<T> ord ) {
+	private Order() {
+		throw new UnsupportedOperationException( "util" );
+	}
+
+	static final class AbecedarianOrder
+			implements Ord<Character> {
+
+		@Override
+		public Ordering ord( Character one, Character other ) {
+			return Ordering.valueOf( one.compareTo( other ) );
+		}
+
+	}
+
+	static final class AlphabeticalOrder
+			implements Ord<CharSequence> {
+
+		@Override
+		public Ordering ord( CharSequence one, CharSequence other ) {
+			final int length = Math.min( one.length(), other.length() );
+			for ( int i = 0; i < length; i++ ) {
+				Ordering ord = abecedarian.ord( one.charAt( i ), other.charAt( i ) );
+				if ( !ord.isEq() ) {
+					return ord;
+				}
+			}
+			return Ordering.EQ;
+		}
+
+	}
+
+	static final class CalendricalOrder
+			implements Ord<Calendar> {
+
+		@Override
+		public Ordering ord( Calendar one, Calendar other ) {
+			return Ordering.valueOf( one.compareTo( other ) );
+		}
+
+	}
+
+	static final class ChronologicalOrder
+			implements Ord<Date> {
+
+		@Override
+		public Ordering ord( Date one, Date other ) {
+			return Ordering.valueOf( one.compareTo( other ) );
+		}
+
+	}
+
+	static final class ComparatorOrder<T>
+			implements Ord<T> {
+
+		final Comparator<T> comparator;
+
+		ComparatorOrder( Comparator<T> comparator ) {
 			super();
-			this.ord = ord;
+			this.comparator = comparator;
 		}
 
 		@Override
-		public int compare( T one, T other ) {
-			return ord.ord( one, other ).intValue();
+		public Ordering ord( T one, T other ) {
+			return Ordering.valueOf( comparator.compare( one, other ) );
+		}
+
+	}
+
+	static final class CompareableOrder<T extends Comparable<T>>
+			implements Ord<T> {
+
+		@Override
+		public Ordering ord( T one, T other ) {
+			return Ordering.valueOf( one.compareTo( other ) );
+		}
+	}
+
+	static final class EnumerativeOrder
+			implements Ord<java.lang.Enum<?>> {
+
+		@Override
+		public Ordering ord( java.lang.Enum<?> one, java.lang.Enum<?> other ) {
+			if ( one.getDeclaringClass() == other.getDeclaringClass() ) {
+				return Ordering.valueOf( one.ordinal() - other.ordinal() );
+			}
+			return alphabetical.ord( one.getClass().getCanonicalName(),
+					other.getClass().getCanonicalName() );
+		}
+
+	}
+
+	static final class HashCodeOrder
+			implements Ord<Object> {
+
+		@Override
+		public Ordering ord( Object one, Object other ) {
+			return Ordering.valueOf( one.hashCode() - other.hashCode() );
+		}
+
+	}
+
+	static final class IdentityOrder
+			implements Ord<Object> {
+
+		@Override
+		public Ordering ord( Object one, Object other ) {
+			return Ordering.valueOf( System.identityHashCode( one )
+					- System.identityHashCode( other ) );
 		}
 
 	}
@@ -126,6 +228,9 @@ public final class Order {
 			if ( one instanceof Character && other instanceof Character ) {
 				return abecedarian.ord( (Character) one, (Character) other );
 			}
+			if ( one instanceof Date && other instanceof Date ) {
+				return chronological.ord( (Date) one, (Date) other );
+			}
 			if ( one instanceof Comparable<?> && other instanceof Comparable<?>
 					&& one.getClass() == other.getClass() ) {
 				return compare( (Comparable) one, (Comparable) other );
@@ -138,27 +243,120 @@ public final class Order {
 		}
 	}
 
-	static final class EnumerativeOrder
-			implements Ord<java.lang.Enum<?>> {
+	static final class InverseOrder<T>
+			implements Ord<T>, Nullproof {
+
+		final Ord<T> ord;
+
+		InverseOrder( Ord<T> ord ) {
+			super();
+			this.ord = ord;
+		}
 
 		@Override
-		public Ordering ord( java.lang.Enum<?> one, java.lang.Enum<?> other ) {
-			if ( one.getDeclaringClass() == other.getDeclaringClass() ) {
-				return Ordering.valueOf( one.ordinal() - other.ordinal() );
+		public boolean isNullsave() {
+			return Null.isSave( ord );
+		}
+
+		@Override
+		public Ordering ord( T one, T other ) {
+			return ord( other, one );
+		}
+	}
+
+	static final class NullsaveOrder<T>
+			implements Ord<T>, Nullsave {
+
+		final Ord<T> ord;
+
+		NullsaveOrder( Ord<T> ord ) {
+			super();
+			this.ord = ord;
+		}
+
+		@Override
+		public final Ordering ord( T one, T other ) {
+			if ( one == null ) {
+				return Ordering.LT;
 			}
-			return alphabetical.ord( one.getClass().getCanonicalName(),
-					other.getClass().getCanonicalName() );
+			if ( other == null ) {
+				return Ordering.GT;
+			}
+			return ord.ord( one, other );
 		}
 
 	}
 
-	static final class IdentityOrder
-			implements Ord<Object> {
+	static final class NumericalOrder
+			implements Ord<Number> {
 
 		@Override
-		public Ordering ord( Object one, Object other ) {
-			return Ordering.valueOf( System.identityHashCode( one )
-					- System.identityHashCode( other ) );
+		public Ordering ord( Number one, Number other ) {
+			if ( one instanceof Float && other instanceof Float ) {
+				return Ordering.valueOf( Float.compare( one.floatValue(), other.floatValue() ) );
+			}
+			if ( one instanceof Long && other instanceof Long ) {
+				return Ordering.valueOf( Long.signum( one.longValue() - other.longValue() ) );
+			}
+			if ( one instanceof Integer && other instanceof Integer ) {
+				return Ordering.valueOf( one.intValue() - other.intValue() );
+			}
+			if ( one instanceof BigDecimal && other instanceof BigDecimal ) {
+				return Ordering.valueOf( ( (BigDecimal) one ).compareTo( (BigDecimal) other ) );
+			}
+			if ( one instanceof BigInteger && other instanceof BigInteger ) {
+				return Ordering.valueOf( ( (BigInteger) one ).compareTo( (BigInteger) other ) );
+			}
+			return Ordering.valueOf( Double.compare( one.doubleValue(), other.doubleValue() ) );
+		}
+
+	}
+
+	static final class OrderComparator<T>
+			implements Comparator<T> {
+
+		private final Ord<T> ord;
+
+		OrderComparator( Ord<T> ord ) {
+			super();
+			this.ord = ord;
+		}
+
+		@Override
+		public int compare( T one, T other ) {
+			return ord.ord( one, other ).intValue();
+		}
+
+	}
+
+	static final class SortableOrder
+			implements Ord<Sortable> {
+
+		@Override
+		public Ordering ord( Sortable one, Sortable other ) {
+			return Ordering.valueOf( one.ordinal() - other.ordinal() );
+		}
+
+	}
+
+	static final class SubOrder<T>
+			implements Ord<T> {
+
+		private final Ord<? super T> primary;
+		private final Ord<? super T> secondary;
+
+		SubOrder( Ord<? super T> primary, Ord<? super T> secondary ) {
+			super();
+			this.primary = primary;
+			this.secondary = secondary;
+		}
+
+		@Override
+		public Ordering ord( T one, T other ) {
+			final Ordering res = primary.ord( one, other );
+			return res.isEq()
+				? secondary.ord( one, other )
+				: res;
 		}
 
 	}
@@ -192,122 +390,5 @@ public final class Order {
 			return Ordering.EQ;
 		}
 
-	}
-
-	static final class NullsaveOrder<T>
-			implements Ord<T>, Nullsave {
-
-		final Ord<T> ord;
-
-		NullsaveOrder( Ord<T> ord ) {
-			super();
-			this.ord = ord;
-		}
-
-		@Override
-		public final Ordering ord( T one, T other ) {
-			if ( one == null ) {
-				return Ordering.LT;
-			}
-			if ( other == null ) {
-				return Ordering.GT;
-			}
-			return ord.ord( one, other );
-		}
-
-	}
-
-	static final class AbecedarianOrder
-			implements Ord<Character> {
-
-		@Override
-		public Ordering ord( Character one, Character other ) {
-			return Ordering.valueOf( one.compareTo( other ) );
-		}
-
-	}
-
-	static final class AlphabeticalOrder
-			implements Ord<CharSequence> {
-
-		@Override
-		public Ordering ord( CharSequence one, CharSequence other ) {
-			final int length = Math.min( one.length(), other.length() );
-			for ( int i = 0; i < length; i++ ) {
-				Ordering ord = abecedarian.ord( one.charAt( i ), other.charAt( i ) );
-				if ( !ord.isEq() ) {
-					return ord;
-				}
-			}
-			return Ordering.EQ;
-		}
-
-	}
-
-	static final class ComparatorOrder<T>
-			implements Ord<T> {
-
-		final Comparator<T> comparator;
-
-		ComparatorOrder( Comparator<T> comparator ) {
-			super();
-			this.comparator = comparator;
-		}
-
-		@Override
-		public Ordering ord( T one, T other ) {
-			return Ordering.valueOf( comparator.compare( one, other ) );
-		}
-
-	}
-
-	static final class CompareableOrder<T extends Comparable<T>>
-			implements Ord<T> {
-
-		@Override
-		public Ordering ord( T one, T other ) {
-			return Ordering.valueOf( one.compareTo( other ) );
-		}
-	}
-
-	static final class NumericalOrder
-			implements Ord<Number> {
-
-		@Override
-		public Ordering ord( Number one, Number other ) {
-			return Ordering.valueOf( Double.compare( one.doubleValue(), other.doubleValue() ) );
-		}
-
-	}
-
-	static final class SortableOrder
-			implements Ord<Sortable> {
-
-		@Override
-		public Ordering ord( Sortable one, Sortable other ) {
-			return Ordering.valueOf( one.ordinal() - other.ordinal() );
-		}
-
-	}
-
-	static final class InverseOrder<T>
-			implements Ord<T>, Nullproof {
-
-		final Ord<T> ord;
-
-		InverseOrder( Ord<T> ord ) {
-			super();
-			this.ord = ord;
-		}
-
-		@Override
-		public Ordering ord( T one, T other ) {
-			return ord( other, one );
-		}
-
-		@Override
-		public boolean isNullsave() {
-			return Null.isSave( ord );
-		}
 	}
 }
