a7a224229e9e964f0a098dd59c612f309e3f8bbf
Jan Bernitt <jan.bernitt@gmx.de>
2012-01-20 16:12:02 +0100

    DOC TailedSequence#tail

--- src/main/java/de/jbee/lang/TailedSequence.java
+++ src/main/java/de/jbee/lang/TailedSequence.java
@@ -4,10 +4,11 @@ public interface TailedSequence<E>
 		extends Sequence<E> {
 
 	/**
-	 * A tail-list of this {@link Sequence}. It depends on this lists internal structure how many
-	 * elements are part of the tail. The tail can be the empty list too. In any case the tail is
-	 * the ending of this list. So it will refer to the same elements in the same sequence as the
-	 * same length sublist at the end of this list.
+	 * A tail-list of this {@link Sequence} omitting an undefined count (>=0) of leading elements.
+	 * 
+	 * It depends on this lists internal structure how many elements are part of the tail. The tail
+	 * may be the empty list too. In any case the tail is the ending of this list. So it will refer
+	 * to the same elements in the same sequence as the same length sublist at the end of this list.
 	 */
 	TailedSequence<E> tail();
 }
